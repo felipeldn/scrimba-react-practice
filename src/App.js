6e4d314f1a1596c967525026f7e143f98b5644af
felipeldn91 <felipeldn91@gmail.com>
2021-06-23 01:51:20 +0100

    ToDoItem component added, some styling done to checkboxes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import NavBar from './components/NavBar.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
+import TodoItem from './components/TodoItem.js'
 
 function App() {
   return (
     <div>
         <NavBar/>
         <Main />
+        <div className="todo-list">
+          <TodoItem/>
+          <TodoItem/>
+          <TodoItem/>
+          <TodoItem/>
+        </div>
         <Footer />
     </div>
   );
