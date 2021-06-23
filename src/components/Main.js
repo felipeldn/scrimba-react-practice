2c8294e063c549f3bf84f02e35b6880b9c9371ae
felipeldn91 <felipeldn91@gmail.com>
2021-06-23 01:31:41 +0100

    displaying and styling data using JSX

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,18 +8,24 @@ function Main() {
   const date = new Date()
   const hours = date.getHours()
   let timeOfDay
+  const styles = {
+    fontSize: 30
+  }
 
   if (hours < 12) {
     timeOfDay = "morning"
+    styles.color = "#04756F"
   } else if (hours >= 12 && hours < 17) {
     timeOfDay = "afternoon"
+    styles.color = "#2E0927"
   } else {
     timeOfDay = "night"
+    styles.color = "#D90000"
   }
 
     return (
     <div>
-      <h1>Good {timeOfDay}</h1>
+      <h1 style={styles}>Good {timeOfDay}</h1>
 
       <p>My name is {`${firstName} ${lasttName}`}</p>
     </div>
