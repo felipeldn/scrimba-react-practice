34acef2c401217b4bd8e3fc565418a896797acdd
felipeldn91 <felipeldn91@gmail.com>
2021-07-19 23:22:51 +0100

    console logs

--- src/App.js
+++ src/App.js
@@ -48,18 +48,4 @@ class App extends React.Component {
     }
 }
 
-// handleChange(id) {
-//   this.setState(prevState => {
-//     const updatedTodos = prevState.todos.map(todo => {
-//       if (todo.id === id) {
-//         todo.completed = !todo.completed
-//       }
-//       return todo
-//     })
-//     return {
-//       todos: updatedTodos
-//     }
-//   })
-// }
-
 export default App;
